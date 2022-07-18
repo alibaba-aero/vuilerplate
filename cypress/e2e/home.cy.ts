51abbb3f4ca2b3721ed5b577348a05e7e6d79ed0
Mohammad <mohammad.hooshdar@yahoo.com>
2022-07-18 18:41:26 +0430

    chore: updates on README.md

--- cypress/e2e/home.cy.ts
+++ cypress/e2e/home.cy.ts
@@ -4,20 +4,20 @@ context('Home', () => {
   })
 
   it('go to test page', () => {
-    cy.url().should('eq', 'http://localhost:3000/')
+    cy.url().should('eq', 'http://localhost:5173/')
 
     cy.getByTestId('test-link')
       .should('have.attr', 'href')
       .and('eq', '/test')
     cy.getByTestId('test-link').click()
 
-    cy.url().should('eq', 'http://localhost:3000/test')
+    cy.url().should('eq', 'http://localhost:5173/test')
 
     cy.getByTestId('home-link')
       .should('have.attr', 'href')
       .and('eq', '/')
     cy.getByTestId('home-link').click()
 
-    cy.url().should('eq', 'http://localhost:3000/')
+    cy.url().should('eq', 'http://localhost:5173/')
   })
 })
